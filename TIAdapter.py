9e16f8cfbb34d2b9c34e3252f6889e722e65b92c
empti <atktgs@gmail.com>
2016-03-27 17:46:52 -0700

    Hello!

---
+++ TIAdapter.py
@@ -0,0 +1,16 @@
+import time
+
+class TIAdapter:
+    __init__(self, name, type):
+        self.__name = name
+        self.__type = type
+        self.__created_time = time()
+        self.__updated_time = __created_time
+        self.__description = ''
+        self.__source = ''
+
+    def update_timestamp(self):
+        self.__updated_time = time()
+
+    def 
+
